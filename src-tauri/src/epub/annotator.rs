a81236fb00c29beca997742c5ac07f575f345a4b
Ange-Emmanuel Kouakou <angemmanuel.kouakou+professional@gmail.com>
2024-07-02 23:22:26 -0400

    feat: Added highlight function
    Simply a function that creates an instance of the Highlight struct.
    
    The app is slowly taking shape in my head, I'd like profiles/accounts to
    make it so even for the same epub different sets of highlights exist.
    This is not a priority though.

--- src-tauri/src/epub/annotator.rs
+++ src-tauri/src/epub/annotator.rs
@@ -4,6 +4,7 @@ As I see it, the take notes is a side feature, so no need to focus on it.
 The extract snippet feature is important for the card creation and would likely be a matter of copying bits of data.
 the highlight might be something I build on top of the extract?
   */
+use crate::mod.rs::EpubDoc;
 
 enum HighlightType{
     Actionable,
@@ -16,11 +17,28 @@ enum HighlightType{
 }
 
 struct Highlight {
-    color: Color,
-    type: HighlightType,
+    highlight_type: HighlightType,
     comment: String,    
     start_cfi:String,
     end_cfi: String,
 }
 
-// Opting for  an external note approach.
+impl Highlight{
+    //TODO: Define the functions to load the highlights from json, save them to Json, and add an highlight and remove one.
+}
+
+fn highlight(highlight_type:HighlightType, text_to_highlight:&str, comment:&str, start_cfi:&str, end_cfi:&str) -> Highlight{
+    Highlight(
+        highlight_type,
+        comment: comment.to_string(),
+        start_cfi: start_cfi.to_string(),
+        end_cfi: end_cfi.to_string()
+    )    
+}
+
+fn highlight_epub(){
+    //TODO: Define this
+}
+
+
+
